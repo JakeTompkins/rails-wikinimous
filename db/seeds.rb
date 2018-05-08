1305e94c53653b73648c6c1218597e054de7bded
Jacob Tompkins <term171013@gmail.com>
2018-05-08 16:55:05 +0800

    finished basic functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+1000.times do
+  a = Article.new()
+  a.title = Faker::ElderScrolls.dragon
+  a.content = Faker::ElderScrolls.race
+  a.save
+end
